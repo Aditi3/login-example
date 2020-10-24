e934b797a6112eeb370d25bbf42aaa69aa04cbec
Aditi3 <aditi.gvc@gmail.com>
2020-10-24 20:28:45 +0530

    chore: init CocoaPods for the Project

---
+++ HelloLogin/Pods/Target Support Files/Pods-HelloLogin-HelloLoginUITests/Pods-HelloLogin-HelloLoginUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloLogin_HelloLoginUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HelloLogin_HelloLoginUITests
+@end
---
+++ HelloLogin/Pods/Target Support Files/Pods-HelloLogin/Pods-HelloLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloLogin : NSObject
+@end
+@implementation PodsDummy_Pods_HelloLogin
+@end
