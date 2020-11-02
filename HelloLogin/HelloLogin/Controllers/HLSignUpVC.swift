be2dbb54f72214c722eb08117ea67ddd3c1b2b5d
Aditi3 <aditi.gvc@gmail.com>
2020-11-02 09:18:52 +0530

    Signup VC enable scrollview on keyboard show

--- HelloLogin/HelloLogin/Controllers/HLSignUpVC.swift
+++ HelloLogin/HelloLogin/Controllers/HLSignUpVC.swift
@@ -14,6 +14,8 @@ class HLSignUpVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var keyboardScrollView: UIScrollView!
+    @IBOutlet weak var signUpHeadlineLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,35 @@ class HLSignUpVC: UIViewController {
         emailTextField.delegate = self
         confirmPasswordTextField.delegate = self
         passwordTextField.delegate = self
+                
+        setUp()
+        addObservers()
+    }
+    
+    func setUp() {
+        signUpHeadlineLabel.numberOfLines = 0
+        signUpHeadlineLabel.text = "Welcome to our\nWorld".uppercased()
+    }
+    
+    func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        
+        guard let userInfo = notification.userInfo else { return }
+        var keyboardFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        var contentInset: UIEdgeInsets = self.keyboardScrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        keyboardScrollView.contentInset = contentInset
+    }
+    
+    @objc func keyboardWillHide(notification:NSNotification) {
+        
+        keyboardScrollView.contentInset = UIEdgeInsets.zero
     }
 }
 
@@ -32,11 +63,16 @@ extension HLSignUpVC: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // Hide the keyboard.
-        nameTextField.resignFirstResponder()
-        emailTextField.resignFirstResponder()
-        passwordTextField.resignFirstResponder()
-        confirmPasswordTextField.resignFirstResponder()
+        textField.resignFirstResponder()
         return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        keyboardScrollView.setContentOffset(CGPoint(x: 0, y: (textField.frame.origin.y - 20)), animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        keyboardScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
     }
     
 }
